a255b4e245c393e97713920b3f5cc0409e3e9f5e
Reimer Backhaus <rbackhaus@gmail.com>
2015-01-05 18:14:09 +0100

    changed license to Unlicense

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ffosrepl "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Unlicense"
+            :url "http://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [aleph "0.4.0-alpha9"]
                  [byte-streams "0.2.0-alpha3"]
