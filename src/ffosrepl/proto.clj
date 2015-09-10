291c2e425f730d61b829ad5f00eda2a0c6c0fdc3
Reimer Backhaus <rbackhaus@gmail.com>
2015-09-10 18:41:03 +0200

    asynchronous receiving from server to allow for ConsoleAPICall messages

--- src/ffosrepl/proto.clj
+++ src/ffosrepl/proto.clj
@@ -22,34 +22,65 @@
     (str len ":" json)))
 
 (defn- receive! [connection]
-  (loop [packet ""]
-    (if-let [byte-data (deref (s/take! connection) 5000 nil)]
-      (let [data (to-string byte-data)
-            new-packet (str packet data)
-            length (packet-length new-packet)
-            json (packet->json new-packet)]
-        (if (> length (count (to-byte-array json)))
-          (recur new-packet)
-          (packet->edn new-packet)))
+  (let [[receive-stream _] connection]
+    (if-let [packet (deref (s/take! receive-stream) 5000 nil)]
+      packet
       (throw (Exception. "timeout while waiting for message from client.")))))
 
 (defn send-sync! [connection data]
-  (let [packet (edn->packet data)
-        success (deref (s/put! connection packet) 5000 nil)]
+  (let [[_ send-stream] connection
+        packet (edn->packet data)
+        success (deref (s/put! send-stream packet) 5000 nil)]
     (if success
       (receive! connection)
       (throw (Exception. "timeout or error while sending to client.")))))
 
+(defn close! [connection]
+  (s/close! (second connection)))
+
+(defn- byte->packet-streams [connection]
+  (let [packet (atom "")
+        packet-stream (s/stream)
+        send-stream (s/stream)]
+    (s/consume (fn [byte-data]
+                 (let [data (to-string byte-data)
+                       new-packet (str @packet data)
+                       length (packet-length new-packet)
+                       json (packet->json new-packet)]
+                   (if (> length (count (to-byte-array json)))
+                     (reset! packet new-packet)
+                     (do
+                       (reset! packet "")
+                       @(s/put! packet-stream (packet->edn new-packet))))))
+               connection)
+    (s/connect send-stream connection)
+    [packet-stream send-stream]))
+
+(defn- print-log-message [msg]
+  (let [args (-> msg
+                 :message
+                 :arguments)]
+    (when args
+      (newline)
+      (apply prn args))))
+
+(defn- setup-connection! [tcp-connection]
+  (let [[connection send-stream] (byte->packet-streams tcp-connection)
+        reply-stream (s/stream)]
+    (s/consume #(if (= (:type %) "consoleAPICall")
+                  (print-log-message %)
+                  (s/put! reply-stream %))
+                   connection)
+    [reply-stream send-stream]))
+
 (defn connect! [host port]
   (let [client (tcp/client {:host host :port port})]
-    (if-let [connection (deref client 5000 nil)]
-      (let [reply (receive! connection)]
+    (if-let [tcp-connection (deref client 5000 nil)]
+      (let [connection (setup-connection! tcp-connection)
+            reply (receive! connection)]
         (when (:applicationType reply)
           connection))
       (throw (Exception. "timeout while connecting to client.")))))
-
-(defn close! [connection]
-  (s/close! connection))
 
 (defn get-webapps-actor! [connection]
   (let [reply (send-sync! connection {:to "root" :type "listTabs"})]
@@ -77,6 +108,9 @@
 (defn get-console-actor! [connection webapps-actor manifest-url]
   (let [reply (send-sync! connection {:to webapps-actor :type "getAppActor" :manifestURL manifest-url})]
     (get-in reply [:actor :consoleActor])))
+
+(defn start-console-listener! [connection console-actor]
+  (send-sync! connection {:to console-actor :type "startListeners" :listeners ["ConsoleAPI"]}))
 
 (defn eval-js! [connection console-actor js]
   (let [reply (send-sync! connection {:to console-actor :type "evaluateJS" :text js})]
